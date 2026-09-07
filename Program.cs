1e3d96677f9e49b093a73151909758372bd0177c
Toasterhog <Rivner2@gmail.com>
2026-09-07 13:00:40 +0200

    lite grejs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("welcome to adveture!");
+
+class Program {
+    static void Main(string[] args) {...}
+    static string Ask(string question) {
+        string response;
+        do {
+            Console.Write(question);
+            response = Console.ReadLine().Trim();
+        } while (response == "");
+        return response;
+    }
+}
