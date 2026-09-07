c76f179ec05538a96b714e122607c3ac6c0c54dd
Toasterhog <Rivner2@gmail.com>
2026-09-07 15:01:01 +0200

    total rewrite

--- Program.cs
+++ Program.cs
@@ -1,26 +1,66 @@
-﻿Console.WriteLine("welcome to adveture!");
-string name = "";
-do
-{
-    Console.Write("What is your name: ");
-    name = Console.ReadLine();
-    
-    Console.Write($"So {name} is truly your name?");
-    string yesOrno = Console.ReadLine().Trim().ToLower();
-    if (yesOrno != "yes" && yesOrno != "ok"){
-        name = "";
-    }
-} while (name == "");
-
+﻿using System.Collections.Generic; 
+MyProgram.GameStart();
 
 class MyProgram {
-    string name = "";
-    static string Ask(string question) {
-        string response;
-        do {
-            Console.Write(question);
-            response = Console.ReadLine().Trim();
-        } while (response == "");
-        return response;
+    static string GetAnswer()
+    {
+        while (true)
+        {
+            string response = Console.ReadLine().Trim();
+            if (response == "" || response == "har inte bestämt mig")
+            {
+                continue;
+            }
+            else
+            {
+                return response;
+            }
+        }
     }
+    static bool AskYesOrNo() {
+        string response = Console.readline();
+        if(response == "yes"){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public static void GameStart()
+    {
+        Character character = new Character();
+        while (character.Location != "End"){
+            if (character.Location == "StartingArea"){
+                StartingArea(character);
+            } 
+            else if (character.Location == "ancient forest"
+            {
+                AncientForest();
+            }
+            else{ Console.Error.Write}
+        }
+    }
+    public static void StartingArea(Character character) {
+        Console.Clear();
+        Console.WriteLine("Welcome to adveture!");
+        do
+        { character.Name = Ask("What is your name, adventurer? "); }
+        while (!AskYesOrNo($"So {character.Name} is truly your name?"));
+
+        character.Loccation = "ancient forest";
+
+    }
+
+    public static void AncientForest()
+    {
+        Console.WriteLine("Welcome to the Ancient forest");
+    }
+}
+
+class Character
+{
+    public string Name;
+    public int Health = 100;
+    public List<string> Items = new List<string>();
+    public string Location = "StartingArea";
 }
