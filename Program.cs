ba5ddaa19a67e5e9b48adbee10804a5f1adf8f43
viktorDull <viktor.kormos7@gmail.com>
2026-09-07 13:23:04 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,18 @@
 ﻿Console.WriteLine("welcome to adveture!");
+string name = "";
+do
+{
+    Console.Write("What is your name: ");
+    name = Console.ReadLine();
+    
+    Console.Write($"So {name} is truly your name?");
+    string yesOrno = Console.ReadLine().Trim().ToLower();
+
+    if (yesOrno != "yes" && yesOrno != "ok"){
+        name = "";
+    }
+} while (name != "");
+
 
 
 class Program {
